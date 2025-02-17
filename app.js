529c41fc8001975ed0328d22fb1532eb47890833
Aman Raj <amanraj.eng111@gmail.com>
2025-02-18 01:39:34 +0530

    home page route changed

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use((req, res, next) => {
 //   res.send(registerUser);
 // });
 
-app.use("/listings", listingRouter);
+app.use("/", listingRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
 
